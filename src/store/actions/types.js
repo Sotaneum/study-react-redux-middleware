bd52522205d2f04f3bdeee7c97930d48bccd27bd
Sotaneum <gnyontu39@gmail.com>
2020-05-07 18:02:35 +0900

    코드 리팩토링

--- src/store/actions/types.js
+++ src/store/actions/types.js
@@ -6,11 +6,9 @@ export const DECREASE_ASYNC = "counter/DECREASE_ASYNC";
 
 export const GET_POST = "sample/GET_POST";
 export const GET_POST_SUCCESS = "sample/GET_POST_SUCCESS";
-export const GET_POST_FAILURE = "sample/GET_POST_FAILURE";
 
 export const GET_USER = "sample/GET_USER";
 export const GET_USER_SUCCESS = "sample/GET_USER_SUCCESS";
-export const GET_USER_FAILURE = "sample/GET_USER_FAILURE";
 
 export const START_LOADING = "loading/START_LOADING";
 export const FINISH_LOADING = "loading/FINISH_LOADING";
--- src/store/reducers/sample.js
+++ src/store/reducers/sample.js
@@ -2,50 +2,15 @@ import {
   GET_POST_SUCCESS,
   GET_USER_SUCCESS,
   GET_POST,
-  GET_POST_FAILURE,
   GET_USER,
-  GET_USER_FAILURE,
 } from "../actions/types";
-import { put, call, takeLatest } from "redux-saga/effects";
+import { takeLatest } from "redux-saga/effects";
 import { handleActions } from "redux-actions";
-import { startLoading, finishLoading } from "../actions/loading";
 import * as api from "../../lib/api";
+import createRequestSaga from "../../lib/createRequestSaga";
 
-function* getPostSaga(action) {
-  yield put(startLoading(GET_POST));
-  try {
-    const post = yield call(api.getPost, action.payload);
-    yield put({
-      type: GET_POST_SUCCESS,
-      payload: post.data,
-    });
-  } catch (e) {
-    yield put({
-      type: GET_POST_FAILURE,
-      payload: e,
-      error: true,
-    });
-  }
-  yield put(finishLoading(GET_POST));
-}
-
-function* getUsersSaga() {
-  yield put(startLoading(GET_USER));
-  try {
-    const users = yield call(api.getUsers);
-    yield put({
-      type: GET_USER_SUCCESS,
-      payload: users.data,
-    });
-  } catch (e) {
-    yield put({
-      type: GET_USER_FAILURE,
-      payload: e,
-      error: true,
-    });
-  }
-  yield put(finishLoading(GET_USER));
-}
+const getPostSaga = createRequestSaga(GET_POST, api.getPost);
+const getUsersSaga = createRequestSaga(GET_USER, api.getUsers);
 
 export function* sampleSaga() {
   yield takeLatest(GET_POST, getPostSaga);
