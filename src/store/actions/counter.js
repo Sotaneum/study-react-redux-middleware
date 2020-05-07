d1502c68432468726a77b7a5ae7b9491a73381f3
Sotaneum <gnyontu39@gmail.com>
2020-05-07 15:42:10 +0900

    redux-thunk 적용 및 loggerMiddleware 제거

--- src/store/actions/counter.js
+++ src/store/actions/counter.js
@@ -3,3 +3,15 @@ import { INCREASE, DECREASE } from "./types";
 
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
+
+export const increaseAsync = () => (dispatch) => {
+  setTimeout(() => {
+    dispatch(increase());
+  }, 1000);
+};
+
+export const decreaseAsync = () => (dispatch) => {
+  setTimeout(() => {
+    dispatch(decrease());
+  }, 1000);
+};
